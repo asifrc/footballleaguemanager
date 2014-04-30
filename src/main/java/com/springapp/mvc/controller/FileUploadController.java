80d41303ff2dacc207cbed77c230dece1595c50f
crowshan <crowshanbin@gmail.com>
2014-04-30 12:01:31 -0500

    #11 | George and Cameron | Added file upload functionality. You can now upload a file containing a list of players (no persistence though)
    TODO: redirect to playerList page (home)

--- src/main/java/com/springapp/mvc/controller/FileUploadController.java
+++ src/main/java/com/springapp/mvc/controller/FileUploadController.java
@@ -1,3 +1,36 @@
 package com.springapp.mvc.controller;
 
-// TODO: write a controller. http://spring.io/guides/gs/uploading-files/
+import com.springapp.mvc.model.Player;
+import com.springapp.mvc.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class FileUploadController {
+    private FileUploadService fileUploadService;
+
+    @Autowired
+    public FileUploadController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public ModelAndView handleUpload(Model model, @RequestParam("file") MultipartFile file) {
+        List<Player> playerList = new ArrayList<Player>();
+        if(!file.isEmpty()) {
+            playerList = fileUploadService.createPlayerList(file);
+        }
+
+        model.addAttribute("playerList", playerList);
+        return new ModelAndView("hello");
+    }
+}
