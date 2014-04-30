c57fa889186145548f259e144de3497aee0c087c
crowshan <crowshanbin@gmail.com>
2014-04-30 14:16:52 -0500

    #11 | George and Cameron | Update HomeTest to reflect new upload functionality and delete redundant test. Update UploadPlayerListTest to test uploadability, rather than outcome.

--- src/test/java/com/springapp/mvc/functional/UploadPlayerListTest.java
+++ src/test/java/com/springapp/mvc/functional/UploadPlayerListTest.java
@@ -6,34 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import java.io.File;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UploadPlayerListTest {
     public static final String BASE_URL = "http://localhost:8080/";
-    public static final String PLAYER_LIST_FILE =
-            "./src/test/java/com/springapp/mvc/playerList.txt";
     WebDriver driver = new HtmlUnitDriver();
 
     @Test
-    public void shouldHaveUploadPlayerListTextFileButton() throws Exception {
+    public void shouldHaveFileUploadRelatedButtons() throws Exception {
         driver.get(BASE_URL);
 
-        WebElement uploadField = driver.findElement(By.id("upload-field"));
-        uploadField.sendKeys(getAbsolutePath(PLAYER_LIST_FILE));
-
+        WebElement browseButton = driver.findElement(By.id("upload-field"));
         WebElement uploadButton = driver.findElement(By.id("upload-button"));
-        uploadButton.click();
 
-        WebElement player_list = driver.findElement(By.id("player-list"));
-
-        assertEquals("Sally", player_list.findElement(By.id("player-1")).getText());
-        assertEquals("Bob", player_list.findElement(By.id("player-2")).getText());
+        assertNotNull(browseButton);
+        assertNotNull(uploadButton);
     }
 
-    private String getAbsolutePath(String filePath) {
-        File file = new File(filePath);
-        return file.getAbsolutePath();
-    }
 }
