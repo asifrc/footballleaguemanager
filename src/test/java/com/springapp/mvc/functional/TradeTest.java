953d2eb63acb0609696407383884e18925fbc79b
crowshan <crowshanbin@gmail.com>
2014-05-13 13:13:12 -0500

    #0 | Cameron | Updated shouldShowADropdownListWithCorrectTeamsNextToEachPlayerOnTradePage to make the correct assertions and to have fewer nested loops

--- src/test/java/com/springapp/mvc/functional/TradeTest.java
+++ src/test/java/com/springapp/mvc/functional/TradeTest.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-import static com.springapp.mvc.functional.FileUploadHelper.PLAYER_FILE_2;
-import static org.junit.Assert.*;
+import static com.springapp.mvc.functional.FileUploadHelper.PLAYER_FILE_1;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TradeTest extends FunctionalBase {
     @Test
     public void shouldBeAbleToGoToTradePlayersPageFromHomePage() {
         driver.get(BASE_URL);
-        helper.uploadFileFor("players", PLAYER_FILE_2);
+        helper.uploadFileFor("players", PLAYER_FILE_1);
 
         List<WebElement> menuItem = driver.findElements(By.className("menuItem"));
         WebElement link = menuItem.get(2);
@@ -27,32 +30,46 @@ public class TradeTest extends FunctionalBase {
         WebElement h1 = driver.findElement(By.tagName("h1"));
         assertEquals("Trade Players", h1.getText());
 
-        assertTrue(driver.findElement(By.id("player-table")).getText().contains("Aubrey"));
+        assertTrue(driver.findElement(By.id("player-table")).getText().contains("Sally"));
     }
 
     @Test
     public void shouldShowADropdownListWithCorrectTeamsNextToEachPlayerOnTradePage() throws Exception {
         driver.get(BASE_URL);
-        helper.uploadFileFor("players", PLAYER_FILE_2);
+        helper.uploadFileFor("players", PLAYER_FILE_1);
 
-        List<WebElement> menuItem = driver.findElements(By.className("menuItem"));
-        WebElement link = menuItem.get(2);
+        List<WebElement> menuItems = driver.findElements(By.className("menuItem"));
+        WebElement link = menuItems.get(2);
 
         assertEquals("Trade Players", link.getText());
 
         link.click();
 
+        List<String> teams = Arrays.asList("Team1", "Team2", "-- Trade --");
+
         List<WebElement> dropDowns = driver.findElements(By.className("team-dropdown"));
 
-        List<String> teams = Arrays.asList("Canadian Argos", "Dallas Cowboys", "Da Behrs", "Barcelona", "Rockets", " -- Trade -- ");
-        for (int i = 0; i < dropDowns.size(); i++) {
-            Select select = new Select(dropDowns.get(i));
-            for (int j = 0; j < teams.size(); j++) {
-                if (i != j) {
-                    assertFalse(select.getOptions().contains(teams.get(j)));
-                }
-            }
-            assertFalse(select.getOptions().contains(teams.get(i)));
+        int i=0;
+        for (WebElement dropDown : dropDowns) {
+            List<String> expectedDropDownOptions = new LinkedList(teams);
+            expectedDropDownOptions.remove(i);
+            List<String> actualDropDownOptions = getDropDownOptionsFrom(dropDown);
+
+            assertEquals(expectedDropDownOptions.size(), actualDropDownOptions.size());
+            assertTrue(actualDropDownOptions.containsAll(expectedDropDownOptions));
+            i++;
         }
+    }
+
+
+
+    private List<String> getDropDownOptionsFrom(WebElement dropDown) {
+        List<String> dropDownOptions = new ArrayList<String>();
+        Select select = new Select(dropDown);
+        for (WebElement dropDownOption : select.getOptions()) {
+            dropDownOptions.add(dropDownOption.getText());
+        }
+
+        return dropDownOptions;
     }
 }
