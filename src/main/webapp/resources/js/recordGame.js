3744b63399c88e9afccc63ed6482ccd5ad377e8d
Asif R Choudhury <choudhury.asif@gmail.com>
2014-05-14 11:28:45 -0500

    #23 | Asif | Refactored javascript to use "pure"[ish] jquery by using a more specific selector

--- src/main/webapp/resources/js/recordGame.js
+++ src/main/webapp/resources/js/recordGame.js
@@ -1,8 +1,7 @@
 $(function () {
     $('.score-team select').on('change', function () {
         $('.score-team select option').attr('disabled', false);
-        var optionInOtherDropdown = $('.score-team select').not($(this))[0].options[this.selectedIndex];
-        $(optionInOtherDropdown).attr('disabled', 'disabled');
+        $('.score-team select:not("#' + this.id + '") :nth-child(' + (this.selectedIndex + 1) + ')').attr('disabled', 'disabled');
         $('.score-team select option:first-child').attr('disabled', false);
     });
 });
