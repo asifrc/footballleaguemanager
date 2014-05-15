72a23b6206ed541e1ae43532d6a86bf6400b387e
crowshan <crowshanbin@gmail.com>
2014-05-15 10:18:16 -0500

    #0 | Cameron | Removed PlayerFactory class

--- src/main/java/com/springapp/mvc/service/PlayerFactory.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.mvc.service;
-
-import com.springapp.mvc.model.Player;
-
-public class PlayerFactory {
-    public Player createFrom(String line) {
-        return null;
-    }
-}
